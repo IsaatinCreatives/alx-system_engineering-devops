3d65eca0bc85ff818f5241b6b99b0972c324ff21
Akintunde Taiwo <aitaiwo01@gmail.com>
2023-05-25 19:51:59 -0700

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""
-Stores data in a csv file.
-"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
-from sys import argv
+import sys
 
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-if __name__ == '__main__':
-    """Stores data in a csv file"""
-    format_user = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
-    format_todo = "https://jsonplaceholder.typicode.com/todos/"
-    response_user = requests.get(format_user).json()
-    response_todo = requests.get(format_todo).json()
-    (name, usrname) = response_user.get('name'), response_user.get('username')
-    (storage, status) = [], {}
-    for item in response_todo:
-        if item.get('userId') == int(argv[1]):
-            status[item.get('title')] = item.get('completed')
-            if item.get('completed'):
-                storage.append("\t " + item.get('title'))
-    with open(f"{argv[1]}.csv", "w", newline="") as cs:
-        writer = csv.writer(cs)
-        #writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-         #                "TASK_TITLE"])
-        for task, complete in status.items():
-            writer.writerow([f'{argv[1]}', f"{usrname}", f"{complete}",
-                             f"{task}"])
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
